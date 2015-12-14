452a1d773bbcd67203258c09d8132c63d15960d3
lynnkuh <kuhlman.lynn@gmail.com>
2015-12-14 13:19:28 -0800

    Commit xib and tableview master

---
+++ KitchenTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KitchenTableViewCell.h
+//  KitchenQuest
+//
+//  Created by Regular User on 12/14/15.
+//  Copyright © 2015 William Cremin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KitchenTableViewCell : UITableViewCell
+
+@end
---
+++ KitchenTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  KitchenTableViewCell.m
+//  KitchenQuest
+//
+//  Created by Regular User on 12/14/15.
+//  Copyright © 2015 William Cremin. All rights reserved.
+//
+
+#import "KitchenTableViewCell.h"
+
+@implementation KitchenTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
